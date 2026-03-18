0f3ec7e9849d4df37f8b45003a2d1e4c7c696c4f
Dinesh Rasal <156992805+DineshRasal@users.noreply.github.com>
2026-03-18 12:04:14 +0530

    Update AuthController.java

--- backend/src/main/java/com/rbac/controller/AuthController.java
+++ backend/src/main/java/com/rbac/controller/AuthController.java
@@ -4,23 +4,23 @@ import com.rbac.dto.AuthResponse;
 import com.rbac.dto.LoginRequest;
 import com.rbac.dto.RegisterRequest;
 import com.rbac.service.AuthService;
-import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Operation; //Used to generate API documentation UI (Swagger)
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
+import jakarta.validation.Valid;                //email not empty
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*; //Main annotations for creating APIs
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/auth")  //THis path for All endpoints in class will start with /api/auth
 @RequiredArgsConstructor
-@Tag(name = "Authentication", description = "Register and Login endpoints")
+@Tag(name = "Authentication", description = "Register and Login endpoints") //Group API for Authentication in swagger
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping("/register")
-    @Operation(summary = "Register a new user")
+    @Operation(summary = "Register a new user")  //@Valid (like not null, email format
     public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
