f10bfa83e5ce6bb205b197d55be0c860cd061b8f
Dinesh Rasal <156992805+DineshRasal@users.noreply.github.com>
2026-03-18 14:27:43 +0530

    Update ProtectedRoute.tsx

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,17 +1,18 @@
-import { Navigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom' //use to redirect user to another page 
 import { useAuth } from '../hooks/useAuth'
 import type { ReactNode } from 'react'
 
 interface Props {
   children: ReactNode
-}
+} 
 
+//use to component to protect routes
 export default function ProtectedRoute({ children }: Props) {
   const { user } = useAuth()
 
   if (!user || !localStorage.getItem('token')) {
     return <Navigate to="/login" replace />
   }
-
+//if user and token is valid the protect page will display by return statement
   return <>{children}</>
 }
