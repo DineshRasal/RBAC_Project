5bd4cebdef95e428d7a6cc8f5bcccdbc7faed881
Dinesh Rasal <156992805+DineshRasal@users.noreply.github.com>
2026-03-18 15:00:09 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react' //StrictMode helps detect errors and bad practices in development
+import { createRoot } from 'react-dom/client'//Used to render your React app into the HTML page.
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// QueryClient → manages API data 
+// QueryClientProvider → makes it available in the whole app
 import './index.css'
 import App from './App'
 
 const queryClient = new QueryClient()
 
-createRoot(document.getElementById('root')!).render(
+//<QueryClientProvider client={queryClient}> useQuery / useMutation will not work
+createRoot(document.getElementById('root')!).render( //render to root file index.html
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}> 
       <App />
     </QueryClientProvider>
   </StrictMode>
