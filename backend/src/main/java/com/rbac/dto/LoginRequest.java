863e364d98e20899760ec61e0fbe55292c9d9607
Dinesh Rasal <156992805+DineshRasal@users.noreply.github.com>
2026-03-18 12:09:14 +0530

    Update LoginRequest.java

--- backend/src/main/java/com/rbac/dto/LoginRequest.java
+++ backend/src/main/java/com/rbac/dto/LoginRequest.java
@@ -1,16 +1,16 @@
 package com.rbac.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank; //These are validation rules to check user input before processing
 import lombok.Data;
 
 @Data
 public class LoginRequest {
 
-    @Email
-    @NotBlank(message = "Email is required")
+    @Email    // Email must not be empty and must be in valid format
+    @NotBlank(message = "Email is required") //If invalid → error handled by GlobalExceptionHandler
     private String email;
 
     @NotBlank(message = "Password is required")
-    private String password;
+    private String password;  //Cannot Be empty
 }
